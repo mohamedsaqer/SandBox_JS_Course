26dcec0b7ccab3df4657540f67f5e9320de538ce
Mohamed Saqer <mohamed.saqer@outlook.com>
2019-11-04 13:02:01 +0200

    using Arrow functions

--- app.js
+++ app.js
@@ -2,46 +2,34 @@ document.querySelector('#button1').addEventListener('click', getText);
 
 function getText(){
     fetch('data.txt')
-        .then(function(responce){
-            return responce.text();
-        })
-        .then(function(data){
-            document.querySelector('#output').innerHTML = data;
-        })
-        .catch(function(err){
-            document.querySelector('#output').innerHTML = err;
-        });
+        .then(responce => responce.text())
+        .then(data  => document.querySelector('#output').innerHTML = data)
+        .catch(err => document.querySelector('#output').innerHTML = err);
 }
 
 document.querySelector('#button2').addEventListener('click', getJSON);
 
 function getJSON() {
     fetch('custmer.json')
-        .then(function (responce) {
-            return responce.json();
-        })
-        .then(function (data) {
+        .then(responce => responce.json())
+        .then(data => {
             let output = '';
-            data.forEach(function(post){
+            data.forEach(post => {
                 output += `
                 <li>${post.title}</li>
                 `;
             });
             document.querySelector('#output').innerHTML = output;
         })
-        .catch(function (err) {
-            console.log(err);
-        });
+        .catch(err => document.querySelector('#output').innerHTML = err);
 }
 
 document.querySelector('#button3').addEventListener('click', getAPI);
 
 function getAPI() {
     fetch('https://api.github.com/users')
-        .then(function (responce) {
-            return responce.json();
-        })
-        .then(function (data) {
+        .then(responce => responce.json())
+        .then(data => {
             let output = '';
             data.forEach(function (post) {
                 output += `
@@ -50,7 +38,5 @@ function getAPI() {
             });
             document.querySelector('#output').innerHTML = output;
         })
-        .catch(function (err) {
-            console.log(err);
-        });
+        .catch(err => document.querySelector('#output').innerHTML = err);
 }
