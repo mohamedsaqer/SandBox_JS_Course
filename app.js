795e4951e349a185c47f442c6e8717cf6f9d03d7
Mohamed Saqer <mohamed.saqer@outlook.com>
2019-11-04 12:07:15 +0200

    Fetch API

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
-document.querySelector('#button').addEventListener('click', loadData);
+document.querySelector('#button1').addEventListener('click', getText);
 
-function loadData() {
-    // Create XHR Object
-    const xhr = new XMLHttpRequest();
+function getText(){
+    fetch('data.txt')
+        .then(function(responce){
+            return responce.text();
+        })
+        .then(function(data){
+            document.querySelector('#output').innerHTML = data;
+        })
+        .catch(function(err){
+            document.querySelector('#output').innerHTML = err;
+        });
+}
 
-    // Open 
-    xhr.open('GET', 'data.txt', true);
+document.querySelector('#button2').addEventListener('click', getJSON);
 
-    // OnLoad
-    xhr.onload = function(){
-        if(this.status === 200){
-            document.querySelector('#output').innerHTML = `<h1> ${this.responseText} </h1>`;
-        }
-    }
+function getJSON() {
+    fetch('custmer.json')
+        .then(function (responce) {
+            return responce.json();
+        })
+        .then(function (data) {
+            let output = '';
+            data.forEach(function(post){
+                output += `
+                <li>${post.title}</li>
+                `;
+            });
+            document.querySelector('#output').innerHTML = output;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
 
-    // Send Request
-    xhr.send();
+document.querySelector('#button3').addEventListener('click', getAPI);
+
+function getAPI() {
+    fetch('https://api.github.com/users')
+        .then(function (responce) {
+            return responce.json();
+        })
+        .then(function (data) {
+            let output = '';
+            data.forEach(function (post) {
+                output += `
+                <li>${post.login}</li>
+                `;
+            });
+            document.querySelector('#output').innerHTML = output;
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
